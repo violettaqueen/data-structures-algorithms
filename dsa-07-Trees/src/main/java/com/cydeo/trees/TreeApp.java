64d281e95596173e157cef1a7f7799097366121e
Violetta <violetta.v.queen@gmail.com>
2022-11-01 21:43:56 -0600

    dsa-trees

--- dsa-07-Trees/src/main/java/com/cydeo/trees/TreeApp.java
+++ dsa-07-Trees/src/main/java/com/cydeo/trees/TreeApp.java
@@ -3,13 +3,22 @@ package com.cydeo.trees;
 public class TreeApp {
     public static void main(String[] args) {
         MyTree tree=new MyTree();
-        int[] numbers=new int[] {10, 5, 12, 3, 6, 11, 13, 2};
-        for (int i = 0; i <8; i++) {
+        int[] numbers=new int[] {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        for (int i = 0; i <11; i++) {
             tree.insert(numbers[i]);
         }
-       // VisualizeTree.printTree(tree.root,null,false);
+       VisualizeTree.printTree(tree.root,null,false);
 
-
+        tree.preOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("In Order Traversal :");
+        tree.inOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("Post Order Traversal: ");
+        tree.postOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("Level Order Traversal: ");
+        tree.levelOrderTraversal();
 
     }
 }
