47eb2f3526e972cf307b1e264860b3eca07b4696
Violetta <violetta.v.queen@gmail.com>
2022-11-08 12:07:40 -0700

    dsa-trees #2

--- dsa-07-Trees/src/main/java/com/cydeo/tree2/BinaryTree.java
+++ dsa-07-Trees/src/main/java/com/cydeo/tree2/BinaryTree.java
@@ -14,17 +14,25 @@ public class BinaryTree {
 
             if (leftMax > rightMax){
                 maxValue = leftMax;
-            }
+            } //otherwise
             maxValue = rightMax;
         }
         if(root.value > maxValue ){
             maxValue = root.value;
         }
         return maxValue;
-
-
     }
 
+    public static boolean searchInBinaryTree(TNode root, int value){
+
+        if (root == null){         //check if root is null
+            return false;
+        }
+        if (root.value == value){ //check if root data == to data to find
+            return true;
+        } // check leftChild, then check rightChild
+        return searchInBinaryTree(root.leftChild, value) || searchInBinaryTree(root.rightChild, value);
+    }
 
 
 
--- dsa-07-Trees/src/main/java/com/cydeo/trees/MyTree.java
+++ dsa-07-Trees/src/main/java/com/cydeo/trees/MyTree.java
@@ -6,42 +6,69 @@ import java.util.Queue;
 public class MyTree {
 
     TNode root;
-    void insert(int value){
 
+    void insert(int value) {
+        TNode newNode = new TNode(value);
+        if (root == null){
+            root = newNode;
+            return;
+        }
+        TNode current = root;
+        while (true){
+            if( value <= current.value){
+
+                if (current.leftChild == null){
+                    current.leftChild = newNode;
+                    break;
+                }
+                current = current.leftChild;
+            }
+            else {
+                if (current.rightChild == null){
+                    current.rightChild = newNode;
+                    break;
+                }
+                //if rightChild id not null, branch into right subtree
+                current = current.rightChild;
+            }
+        }
     }
-    void preOrderTraversal(TNode root){
+
+    void preOrderTraversal(TNode root) {
 
         // recursive algorithm / RECURSION
 
+        if (root == null) return;  //termination
+        System.out.println(root.value);  //visit root
+        preOrderTraversal(root.leftChild);  //visit left subtree
+        preOrderTraversal(root.rightChild); //visit right subtree
+    }
+
+    void inOrderTraversal(TNode root) {
+
         if (root == null) return;
-        System.out.println(root.value);
-        preOrderTraversal(root.leftChild);
-        preOrderTraversal(root.rightChild);
-    }
-
-    void inOrderTraversal(TNode root){
-
-        if( root == null) return;
-        inOrderTraversal(root.leftChild);
+        inOrderTraversal(root.leftChild);  //go to left until null
         System.out.println(root.value + ", ");
-        inOrderTraversal(root.rightChild);
+        inOrderTraversal(root.rightChild); //branch into the right
     }
 
-    void postOrderTraversal(TNode root){
+    void postOrderTraversal(TNode root) {
 
-        if (root == null){
+        if (root == null) {
             return;
         }
-        postOrderTraversal(root.leftChild);
-        postOrderTraversal(root.rightChild);
-        System.out.println(root.value + ", ");
+        postOrderTraversal(root.leftChild); // branch into left subtree
+        postOrderTraversal(root.rightChild); //branch and finish right subtree
+        System.out.println(root.value + ", "); //visit root
     }
-    void levelOrderTraversal(){
+
+    void levelOrderTraversal() {  //use Queue if we need in order elements
         if (root == null) return;
         Queue<TNode> queue = new LinkedList<>();
         queue.add(root);
-        while (!queue.isEmpty()){
-            TNode toVisit = queue.poll();
+        while (!queue.isEmpty()) {  //until Queue becomes empty
+            TNode toVisit = queue.poll(); //
+            //if you have n number of children
             if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
             if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
         }
