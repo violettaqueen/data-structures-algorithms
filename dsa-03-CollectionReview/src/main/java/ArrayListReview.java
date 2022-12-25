357fbe357a9449f98ef69c3fe3ba51d80f26add2
Violetta <violetta.v.queen@gmail.com>
2022-12-25 07:17:52 -0700

    Array

--- dsa-03-CollectionReview/src/main/java/ArrayListReview.java
+++ dsa-03-CollectionReview/src/main/java/ArrayListReview.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class ArrayListReview {
     public static void main(String[] args) {
         // Create ArrayList and a class
-   List<Student> students=new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
 // Add elements to ArrayList
-        students.add(new Student(1,"Jack"));
-        students.add(new Student(2,"Julia"));
-        students.add(new Student(3,"Mike"));
-        students.add(new Student(4,"Mary"));
+        students.add(new Student(1, "Jack"));
+        students.add(new Student(2, "Julia"));
+        students.add(new Student(3, "Mike"));
+        students.add(new Student(4, "Mary"));
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
@@ -21,21 +21,21 @@ public class ArrayListReview {
         // 2. Iterator
         // Forward Iteration
         System.out.println("Printing with iterator..............");
-        Iterator iter= students.listIterator();
-        while(iter.hasNext()){                        //
+        Iterator iter = students.listIterator();
+        while (iter.hasNext()) {                        //
             System.out.println(iter.next());
         }
 
         // Backwards Iteration
         System.out.println("Printing Backwards with iterator..............");
 
-        while(((ListIterator<Student>) iter).hasPrevious()){
-            System.out.println(((ListIterator<Student>) iter).previous());
+        while (((ListIterator<?>) iter).hasPrevious()) {
+            System.out.println(((ListIterator<?>) iter).previous());
         }
 
         // 3. for each loop
         System.out.println("Printing with for_each loop..............");
-        for (Student s:students) {
+        for (Student s : students) {
             System.out.println(s);
         }
 
@@ -54,14 +54,16 @@ public class ArrayListReview {
         students.forEach(System.out::println);
 
     }
-      //  implementations of  Comparator
+
+    //  implementations of  Comparator
     static class sortByIdDesc implements Comparator<Student> {
 
-          @Override
-          public int compare(Student o1, Student o2) {
-              return o2.id-o1.id;
-          }
-      }
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.id - o1.id;
+        }
+    }
+
     static class sortByNameDesc implements Comparator<Student> {
 
         @Override
